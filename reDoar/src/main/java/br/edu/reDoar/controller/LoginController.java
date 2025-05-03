bed62c7cf9448cea75d760aa83c5c04953bb05cc
Roberto Campos <robertoneto.campos@gmail.com>
2025-05-03 17:18:48 -0300

    Add files via upload

---
+++ reDoar/src/main/java/br/edu/reDoar/controller/LoginController.java
@@ -0,0 +1,13 @@
+package br.edu.reDoar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String Login() { return "telaLogin"; // procura por templates/telaLogin.html
+    }
+
+}
