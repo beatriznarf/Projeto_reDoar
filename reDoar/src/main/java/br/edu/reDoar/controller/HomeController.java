4e2b04e8fdaf953c12fd88ff076bac0a3f6f38a2
Beatriz Narciso <147112758+beatriznarf@users.noreply.github.com>
2025-05-25 18:11:10 -0300

    Alterações - QR Code
    QR Code único para as doações;

--- reDoar/src/main/java/br/edu/reDoar/controller/HomeController.java
+++ reDoar/src/main/java/br/edu/reDoar/controller/HomeController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/telainicial"})
-    public String telaInicial() {
-        return "telainicial";
-    }
-
     @GetMapping("/SaibaMais")
     public String telaSaibaMais() {
         return "telaSaibaMais";
---
+++ reDoar/src/main/java/br/edu/reDoar/controller/TelaInicialController.java
@@ -0,0 +1,14 @@
+package br.edu.reDoar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TelaInicialController {
+
+    @GetMapping({"/", "/telainicial"})
+    public String telaInicial() {
+        return "telainicial";
+    }
+
+}
