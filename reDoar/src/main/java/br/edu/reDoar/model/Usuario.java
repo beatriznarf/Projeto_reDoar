0ef83de093b8d5ce7534efac60f33bf19fefc3f5
Beatriz Narciso <147112758+beatriznarf@users.noreply.github.com>
2025-05-25 11:44:12 -0300

    insert funcionarios ajustado
    Foi feito um ajuste na criação dos dados dos funcionários, porque o sistema estava inserindo automaticamente um primeiro usuário com ID 1. Quando tentávamos cadastrar outro funcionário, dava erro, pois o sistema achava que ainda era o ID 1, em vez de seguir para o ID 2.

--- reDoar/src/main/java/br/edu/reDoar/model/Usuario.java
+++ reDoar/src/main/java/br/edu/reDoar/model/Usuario.java
@@ -9,6 +9,8 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String nome;
     private String cpf;
     private String email;
