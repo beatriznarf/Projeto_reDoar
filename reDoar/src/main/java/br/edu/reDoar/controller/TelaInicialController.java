794e5133de217c4da874fe12d1db53e50bc52e56
robertonetoz <robertoneto.campos@gmail.com>
2025-06-02 17:42:32 -0300

    mudanças no controller

--- reDoar/src/main/java/br/edu/reDoar/controller/TelaInicialController.java
+++ reDoar/src/main/java/br/edu/reDoar/controller/TelaInicialController.java
@@ -8,7 +8,7 @@ public class TelaInicialController {
 
     @GetMapping({"/", "/telainicial"})
     public String telaInicial() {
-        return "telainicial";
+        return "telaInicial";
     }
 
 }
