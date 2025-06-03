4e4a59b72cdade9ca8b389cd0410792a4483a5d4
robertonetoz <robertoneto.campos@gmail.com>
2025-06-03 12:04:12 -0300

    rotas e controller da tela inicial

--- reDoar/src/main/java/br/edu/reDoar/controller/TelaInicialController.java
+++ reDoar/src/main/java/br/edu/reDoar/controller/TelaInicialController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TelaInicialController {
 
-    @GetMapping({"/", "/telainicial"})
+    @GetMapping({"/", "/telaInicial"})
     public String telaInicial() {
         return "telaInicial";
     }
